bfb9ff1c8806ec1a01b28cb03efea0867aaeeb8b
Anastasia Epifanik <nastyepifanik@gmail.com>
2019-01-09 15:52:08 +0200

    add Event.java

---
+++ src/main/java/com/netcracker/superproject/entity/Event.java
@@ -0,0 +1,99 @@
+package com.netcracker.superproject.entity;
+
+import com.netcracker.superproject.entity.annotations.Attribute;
+import com.netcracker.superproject.entity.annotations.Entity;
+
+@Entity(type = "2")
+public class Event extends BaseEntity {
+
+    @Attribute(type = "2001")
+    private String title;
+
+    @Attribute(type = "2002")
+    private String description;
+
+    @Attribute(type = "2003")
+    private String location;
+
+    @Attribute(type = "2004")
+    private String contacts;
+
+    @Attribute(type = "2005")
+    private String tags;
+
+    @Attribute(type = "2006")
+    private String photos;
+
+    @Attribute(type = "2007")
+    private String orginizer_id;
+
+    @Attribute(type = "2008")
+    private String polls_id;
+
+    public Event() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    public String getOrginizer_id() {
+        return orginizer_id;
+    }
+
+    public void setOrginizer_id(String orginizer_id) {
+        this.orginizer_id = orginizer_id;
+    }
+
+    public String getPolls_id() {
+        return polls_id;
+    }
+
+    public void setPolls_id(String polls_id) {
+        this.polls_id = polls_id;
+    }
+}
