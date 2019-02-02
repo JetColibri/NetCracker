fa6bf7744c8c99e5400538a4a0ddb921627c2272
JetColibri <m.v.zinkovskiy@gmail.com>
2019-02-02 12:40:03 +0200

    *reference

---
+++ src/main/java/com/netcracker/superproject/entity/annotations/Reference.java
@@ -0,0 +1,9 @@
+package com.netcracker.superproject.entity.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Reference {
+    String type();
+}
